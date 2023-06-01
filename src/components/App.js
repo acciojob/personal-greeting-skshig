bc2ee1723c544a5ea96838c3de309743e21493bb
skshig <skshig@gmail.com>
2023-06-01 21:34:04 +0530

    personal greeting

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [userName, setUserName] = useState("");
   return (
     <div>
+      <form action="">
         {/* Do not remove the main div */}
+        {/* <label htmlFor="name">Enter your name: </label> */}
+        <input type="text" onChange={(e)=>setUserName(e.target.value)}/>
+        </form>
+        <p>Hello {userName}</p>
     </div>
   )
 }
